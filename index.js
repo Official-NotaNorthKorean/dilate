fd7aea8e5cd9264111a478c8cb4e00d8434b4a66
NotaNorthKorean <admin@notanorthkorean.ca>
2021-10-24 14:48:16 -0700

    Changed CSS; buttons in main page do not align properly 	modified:   index.css 	modified:   index.html 	modified:   index.js 	modified:   login/index.css 	modified:   login/index.html

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 function openTab(evt, tabName) {
 	var i, tabcontent, tabButtons;
 	tabcontent = document.getElementsByClassName("tabcontent");
-	for (i = 0; i < tabcontent.length; i++) {
-		tabcontent[i].style.display = "none";
+		for (i = 0; i < tabcontent.length; i++) {
+			tabcontent[i].style.display = "none";
 	}
 	tabButtons = document.getElementsByClassName("tabButtons");
-	for (i = 0; i < tabButtons.length; i++) {
-		tabButtons[i].className = tabButtons[i].className.replace(" active", "");
+		for (i = 0; i < tabButtons.length; i++) {
+			tabButtons[i].className = tabButtons[i].className.replace("active", "");
 	}
-/*	document.getElementById(tabName).style.display = "flex";*/
 	document.getElementById(tabName).style = "display: flex; flex-direction: column; height: 100%;";
 	evt.currentTarget.className += "active";
+
+/*	document.getElementsByClassName("defaultTab");
+		for (i = 0; i < */
 }
