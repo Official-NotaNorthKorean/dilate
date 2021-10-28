e5bff188e09d842be8373f953826bc4bb65a1a3a
NotaNorthKorean <admin@notanorthkorean.ca>
2021-10-27 17:30:23 -0700

    Fixed button JavaScript 	modified:   TODO.md 	modified:   index.css 	modified:   index.html 	modified:   index.js

--- TODO.md
+++ TODO.md
@@ -5,3 +5,4 @@ TO DO
 -	Create UI for MUC rooms and direct messages
 -	Create settings menu
 -	Create help menu
+-	The style of a button is reset when pressed; fix this [ ✔ ]
--- index.js
+++ index.js
@@ -1,26 +1,10 @@
-function openTab(evt, tabName) {
-	var i, tabcontent, tabButtons;
-	tabcontent = document.getElementsByClassName("tabcontent");
-		for (i = 0; i < tabcontent.length; i++) {
-			tabcontent[i].style.display = "none";
+function showStuff(element) {
+	var tabContents = document.getElementsByClassName('tabContent');
+	for (var i = 0; i < tabContents.length; i++) { 
+		tabContents[i].style.display = 'none';
 	}
-	tabButtons = document.getElementsByClassName("tabButtons", "buddybutton");
-		for (i = 0; i < tabButtons.length; i++) {
-			tabButtons[i].className = tabButtons[i].className.replace("active");
-	}
-	document.getElementById(tabName).style = "display: flex; flex-direction: column; height: 100%;";
-	evt.currentTarget.className += "active";
-}
-function openNavTab(evt, tabName) {
-	var i, navTabContent, navButtons;
-	navTabContent = document.getElementsByClassName("navTabContent");
-		for (i = 0; i < navTabContent.length; i++) {
-			navTabContent[i].style.display = "none";
-		}
-	navButtons = document.getElementsByClassName("gohome", "newmuc");
-		for (i = 0; i < navButtons.length; i++) {
-			navButtons[i].className = navButtons[i].className.replace("active");
-		}
-	document.getElementById(tabName).style = "display: flex; flex-direction: column; height: 100%;";
-	evt.currentTarget.className += "active";
+
+	// change tabsX into tabs-X in order to find the correct tab content
+	var tabContentIdToShow = element.id.replace(/(\d)/g, '-$1');
+	document.getElementById(tabContentIdToShow).style.display = 'block';
 }
