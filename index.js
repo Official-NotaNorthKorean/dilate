7160f9b193de58d2481b2de86479b485c1ce2e36
NotaNorthKorean <admin@notanorthkorean.ca>
2021-10-28 12:20:34 -0700

    Added `nav` tabs 	new file:   addmuc.css 	modified:   index.css 	modified:   index.html 	modified:   index.js 	new file:   muc.css 	new file:   primecontent.css

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ function primeTopShowTab(element) {
 	var tabContentIdToShow = element.id.replace(/(\d)/g, '-$1');
 	document.getElementById(tabContentIdToShow).style.display = 'flex';
 }
-/*
+
 function navShow(element) {
-	var tabContents = document.getElementsByClassName('tabContent');
+	var tabContents = document.getElementsByClassName('navContent');
 	for (var i = 0; i < tabContents.length; i++) { 
 		tabContents[i].style.display = 'none';
 	}
 	// change tabsX into tabs-X in order to find the correct tab content
 	var tabContentIdToShow = element.id.replace(/(\d)/g, '-$1');
-	document.getElementById(tabContentIdToShow).style.display = 'flex';
-} */
+	document.getElementById(tabContentIdToShow).style = 'display: block; left: 72px; top: 0px; right: 0px; height: 100%; position: fixed; background-color: #36393f; overflow: hidden;';
+}
